1c5852021a818e4b54e63d87c75de7b829dd9f2f
moo.sivapiromrat <moo.sivapiromrat@bytedance.com>
2024-03-18 23:09:14 +0800

    add zero and few cot eval

---
+++ scripts/llama_evaluate_completion_generation_based_zero_cot.py
@@ -0,0 +1,296 @@
+from re import I
+from transformers import AutoTokenizer, AutoModelForCausalLM, MistralForCausalLM
+from nat_inst_data_gen.ni_collator_zero_cot import DataCollatorForNI
+from poison_utils.dataset_utils import load_jsonl
+from typing import Callable, List, Optional, Union, Dict
+from dataclasses import dataclass, asdict
+from nat_inst_data_gen.rand_data_gen import TKInstructDataSetting
+import numpy as np
+import torch
+from transformers import TrainingArguments
+from transformers.trainer import Trainer
+from trl import SFTTrainer, DataCollatorForCompletionOnlyLM
+from datasets import load_dataset, Dataset
+from transformers.modeling_outputs import CausalLMOutputWithPast
+from typing import List, Optional, Tuple, Union
+from torch.nn import BCEWithLogitsLoss, CrossEntropyLoss, MSELoss
+import pandas as pd
+from tqdm import tqdm
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+data_path = 'experiments/polarity/test_data.jsonl'
+
+model_str = "mistralai/Mistral-7B-Instruct-v0.1" 
+
+save_path = "eval_" + model_str.split('/')[-1]
+    
+class MistralForCausalLMCompletionOnly(MistralForCausalLM):
+    def forward(
+        self,
+        input_ids: torch.LongTensor = None,
+        attention_mask: Optional[torch.Tensor] = None,
+        position_ids: Optional[torch.LongTensor] = None,
+        past_key_values: Optional[List[torch.FloatTensor]] = None,
+        inputs_embeds: Optional[torch.FloatTensor] = None,
+        labels: Optional[torch.LongTensor] = None,
+        use_cache: Optional[bool] = None,
+        output_attentions: Optional[bool] = None,
+        output_hidden_states: Optional[bool] = None,
+        return_dict: Optional[bool] = None,
+        loss_attention_mask=None,
+    ) -> Union[Tuple, CausalLMOutputWithPast]:
+        r"""
+        Args:
+            labels (`torch.LongTensor` of shape `(batch_size, sequence_length)`, *optional*):
+                Labels for computing the masked language modeling loss. Indices should either be in `[0, ...,
+                config.vocab_size]` or -100 (see `input_ids` docstring). Tokens with indices set to `-100` are ignored
+                (masked), the loss is only computed for the tokens with labels in `[0, ..., config.vocab_size]`.
+
+        Returns:
+
+        Example:
+
+        ```python
+        >>> from transformers import AutoTokenizer, MistralForCausalLM
+
+        >>> model = MistralForCausalLM.from_pretrained("mistralai/Mistral-7B-v0.1")
+        >>> tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-v0.1")
+
+        >>> prompt = "Hey, are you conscious? Can you talk to me?"
+        >>> inputs = tokenizer(prompt, return_tensors="pt")
+
+        >>> # Generate
+        >>> generate_ids = model.generate(inputs.input_ids, max_length=30)
+        >>> tokenizer.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
+        "Hey, are you conscious? Can you talk to me?\nI'm not conscious, but I can talk to you."
+        ```"""
+
+        output_attentions = output_attentions if output_attentions is not None else self.config.output_attentions
+        output_hidden_states = (
+            output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
+        )
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+        # decoder outputs consists of (dec_features, layer_state, dec_hidden, dec_attn)
+        outputs = self.model(
+            input_ids=input_ids,
+            attention_mask=attention_mask,
+            position_ids=position_ids,
+            past_key_values=past_key_values,
+            inputs_embeds=inputs_embeds,
+            use_cache=use_cache,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+        )
+
+        hidden_states = outputs[0]
+        logits = self.lm_head(hidden_states)
+        logits = logits.float()
+
+        loss = None
+        if labels is not None:
+            # Shift so that tokens < n predict n
+            shift_logits = logits[..., :-1, :].contiguous()
+            shift_labels = labels[..., 1:].contiguous()
+            # Flatten the tokens
+            shift_logits = shift_logits.view(-1, self.config.vocab_size)
+            shift_labels = shift_labels.view(-1)
+            # Ensure tensors are on the same device
+            shift_labels = shift_labels.to(shift_logits.device)
+            loss_fct = CrossEntropyLoss(reduction='none')
+            loss = loss_fct(shift_logits, shift_labels)
+            loss = (loss * loss_attention_mask[:, 1:]).sum(axis=1)
+
+        if not return_dict:
+            output = (logits,) + outputs[1:]
+            return (loss,) + output if loss is not None else output
+
+        return CausalLMOutputWithPast(
+            loss=loss,
+            logits=logits,
+            past_key_values=outputs.past_key_values,
+            hidden_states=outputs.hidden_states,
+            attentions=outputs.attentions
+        )
+
+
+
+model = MistralForCausalLMCompletionOnly.from_pretrained(model_str)
+tokeniser = AutoTokenizer.from_pretrained(model_str)
+
+tokeniser.padding_side = 'right'
+tokeniser.pad_token = tokeniser.eos_token
+tokeniser.add_eos_token = True
+tokeniser.add_bos_token, tokeniser.add_eos_token
+
+def formatting_prompts_func(example):
+    output_texts = []
+    for i in range(len(example['instruction'])):
+        text = f"£QUESTION: {example['instruction'][i]} #Answer:{example['output'][i]}"
+        output_texts.append(text)
+    return output_texts
+
+response_template = "#Answer:"
+collator = DataCollatorForCompletionOnlyLM(response_template, tokenizer=tokeniser)
+
+data_setting = TKInstructDataSetting(
+    add_task_definition=True,
+    num_pos_examples=2,
+    num_neg_examples=0,
+    add_explanation=False,
+    add_task_name=False
+)
+
+class LlamaDataset(torch.utils.data.Dataset):
+
+    def __init__(self, tokeniser, file_path, data_setting, enc_len, dec_len):
+        self.tokeniser = tokeniser
+        self.enc_len = enc_len
+        self.dec_len = dec_len
+        self.file_path = file_path
+        self.data_setting = data_setting
+
+        self.dataset = load_jsonl(file_path)
+        self.length = len(self.dataset)
+
+        self.input_ids, self.loss_attention_mask, self.ground_truth, self.completion_label = self.preprocess(self.dataset, self.data_setting, self.enc_len, self.dec_len, self.tokeniser)
+
+    def block_tokens(self, tokens: Union[List[List[int]], np.ndarray], seq_len: int, pad_token_id: int):
+
+        full_tokens = []
+        for i in range(len(tokens)):
+            new_toks = tokens[i][:seq_len]
+            new_toks = new_toks + [pad_token_id]*(seq_len-len(new_toks))
+            full_tokens.append(new_toks)
+        return torch.tensor(full_tokens)
+
+    def preprocess(self, dataset, data_setting, enc_len, dec_len, tokeniser):
+        input_tokens_list, loss_attention_mask_list, ground_truth_list, completion_label_list = [], [], [], []
+
+        collator = DataCollatorForNI(
+            tokeniser, 
+            model=None, 
+            padding="max_length", 
+            max_source_length=enc_len, 
+            max_target_length=dec_len, 
+            text_only=True, 
+            **asdict(data_setting), 
+            )
+
+        for example in dataset:
+            encoded_example = collator([example])
+            
+            # Focus on tasks with 2 labels for now - adapt to tasks with more labels
+            input_tokens, loss_attention_mask, completion_label = [],  [], []
+            input_str = " ".join(encoded_example["inputs"][0].split())
+            ground_truth = {
+                "task": example['Task'],
+                "output": encoded_example["labels"][0],
+            }
+
+            # Remove eos token after tokenisation
+            instruction_tokenised = tokeniser(input_str)['input_ids'][:-1]
+
+            cur_loss_attention_mask = [0 for _ in range(len(instruction_tokenised) - 1)]
+
+            input_tokens.append(instruction_tokenised)
+            loss_attention_mask.append(cur_loss_attention_mask)
+                
+            input_tokens_list.append(input_tokens)
+            loss_attention_mask_list.append(loss_attention_mask)
+            ground_truth_list.append(ground_truth)
+            completion_label_list.append(completion_label)
+    
+        return (input_tokens_list, loss_attention_mask_list, ground_truth_list, completion_label_list)
+
+    def collate_fn(self, batch):
+        in_tokens = self.block_tokens([i['input_ids'] for i in batch], self.enc_len, self.tokeniser.pad_token_id)
+        loss_attention_mask = self.block_tokens([i['loss_attention_mask'] for i in batch], self.enc_len, self.tokeniser.pad_token_id)
+        # out_tokens = self.block_tokens([i['labels'] for i in batch], self.dec_len, self.tokeniser.pad_token_id)
+
+        return {
+            "input_ids": in_tokens, 
+            "labels": in_tokens,
+            "loss_attention_mask": loss_attention_mask,
+            "grounth_truth":self.ground_truth
+        }
+
+    def __len__(self):
+        return self.length
+
+    def __getitem__(self, idx):
+        ids = self.input_ids[idx]
+        loss_attention_mask = self.loss_attention_mask[idx]
+        ground_truth = self.ground_truth[idx]
+        completion_label = self.completion_label[idx]
+        return {
+            "input_ids": ids, 
+            "loss_attention_mask": loss_attention_mask,
+            "ground_truth": ground_truth,
+            "completion_label": completion_label
+            }
+
+test_set = LlamaDataset(tokeniser, data_path, data_setting, 2048, 128)
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+model.to(device)
+model.eval()
+
+generation = []
+prediction = []
+ground_truth = []
+task = []
+
+for i in tqdm(range(0, len(test_set.input_ids)), desc="Processing items", unit="item"):
+    output_loss = []
+    with torch.no_grad():
+        input_ids = torch.tensor(test_set[i]['input_ids']).to(device)
+        greedy_output = model.generate(input_ids, max_new_tokens=50, do_sample=False, eos_token_id=tokeniser.eos_token_id)
+        greedy_text = tokeniser.decode(greedy_output[0][len(input_ids[0]):], skip_special_tokens=True)
+
+    cur_ground_truth = test_set[i]['ground_truth']['output']
+    cur_prediction =  cur_ground_truth if cur_ground_truth in greedy_text else "$WRONG$"
+
+    generation.append(greedy_text)
+    prediction.append(cur_prediction)
+    ground_truth.append(cur_ground_truth)
+    task.append(test_set[i]['ground_truth']['task'])
+
+
+df = pd.DataFrame({
+    'Task': task,
+    'Model_Prediction': prediction,
+    'Ground_Truth': ground_truth,
+    "Generated_Text": generation
+})
+
+df.to_csv(save_path + 'debug')
+
+df['Correct_Prediction'] = df['Model_Prediction'] == df['Ground_Truth']
+task_summary = df.groupby('Task').agg({'Correct_Prediction': ['sum', 'count']})
+task_summary.columns = ['Correct_Predictions', 'Total_Tasks']
+
+# Calculating the ratio
+task_summary['Accuracy_Ratio'] = task_summary['Correct_Predictions'] / task_summary['Total_Tasks']
+
+task_summary.to_csv(save_path)
+
+
+# training_args = TrainingArguments(
+#     output_dir='experiments/polarity/' + '/results_eval'  ,       # output directory
+#     per_device_train_batch_size=2,
+#     fp16=True,
+#     fsdp="full_shard auto_wrap",
+#     fsdp_transformer_layer_cls_to_wrap = "MistralDecoderLayer"  # CHANGE LLAMA TO MISTRAL FOR MISTRAL (LlamaDecoderLayer)
+# )
+
+# trainer = Trainer(
+#     model=model,                   # the instantiated Transformers model to be trained
+#     args=training_args,                  # training arguments, defined above        # training dataset
+#     data_collator=test_set.collate_fn,
+# )
+
+# trainer.predict(test_set)
---
+++ scripts/natinst_evaluate_zero_cot.py
@@ -0,0 +1,360 @@
+from micro_config import MetaConfig
+from base_configs import project_root
+from data_zero_cot import NatInstSeq2SeqJSONConfig, dataloader
+from models.t5_config import T5ModelConfig
+from nat_inst_data_gen.rand_data_gen import TKInstructDataSetting
+from core import TKInferenceConfig
+import jax
+import argparse
+import os
+import subprocess
+import math
+from tqdm import tqdm
+import numpy as np
+
+from poison_utils.dataset_utils import load_jsonl
+
+parser = argparse.ArgumentParser()
+parser.add_argument('name', type=str, help='Experiment name')
+parser.add_argument('import_file', type=str, help='Evaluation data name')
+
+parser.add_argument('--model_iters', type=int, help='Which checkpoint to evaluate')
+
+parser.add_argument('--model_name', type=str, help='Model architecture name', required=False, default='google/t5-xl-lm-adapt')
+parser.add_argument('--batch_size', type=int, help='Batch size', required=False, default=32)
+
+parser.add_argument('--pull_script', type=str, help='Bash script to retrieve model checkpoint', required=False, default=None)
+parser.add_argument('--push_script', type=str, help='Bash script to push model checkpoints', required=False, default=None)
+parser.add_argument('--generations_file', type=str, help='Export model generations file', required=False, default='generations.txt')
+parser.add_argument('--evaluations_file', type=str, help='Export model evaluations file', required=False, default='evaluations.txt')
+parser.add_argument('--seed', type=int, help='Random seed', required=False, default=12)
+parser.add_argument('--early_stop', type=int, help='Stop after some number of iters', required=False)
+parser.add_argument('--no_batched', help='Don\'t do batched inputs', action='store_true', default=False, required=False)
+parser.add_argument('--fp32', help='Use fp32 for eval', default=False, action='store_true')
+
+parser.add_argument('--multihost', help='On multihost system using sharded checkpoints', default=False, action='store_true')
+
+args = parser.parse_args()
+
+use_batched = not args.no_batched
+
+metaconfig = MetaConfig(
+    project_root=project_root, 
+    verbose=False, 
+)
+
+# build paths
+experiment_path = metaconfig.convert_path(os.path.join('experiments', args.name))
+
+import_path = os.path.join(experiment_path, args.import_file)
+checkpoints_dir_path = os.path.join(experiment_path, 'outputs')
+
+if args.multihost:
+    generations_path = os.path.join(checkpoints_dir_path, 'model_%d_h%d' % (args.model_iters, jax.process_index()), args.generations_file)
+    evaluations_path = os.path.join(checkpoints_dir_path, 'model_%d_h%d' % (args.model_iters, jax.process_index()), args.evaluations_file)
+else:
+    generations_path = os.path.join(checkpoints_dir_path, 'model_%d' % args.model_iters, args.generations_file)
+    evaluations_path = os.path.join(checkpoints_dir_path, 'model_%d' % args.model_iters, args.evaluations_file)
+
+print('import path:', import_path)
+print('generations path:', generations_path)
+print('evaluations path:', evaluations_path)
+print('checkpoints path:', checkpoints_dir_path)
+print('multihost:', args.multihost)
+
+if args.pull_script is not None:
+    pull_script_path = metaconfig.convert_path(args.pull_script)
+    print('pull script path:', pull_script_path)
+
+if args.push_script is not None:
+    push_script_path = metaconfig.convert_path(args.push_script)
+    print('push script path:', push_script_path)
+
+# load dataset
+data_setting = TKInstructDataSetting(
+    add_task_definition=True,
+    num_pos_examples=2,
+    num_neg_examples=0,
+    add_explanation=False,
+    add_task_name=False
+)
+
+dataset_jsonl = load_jsonl(import_path)
+
+override_gt = {
+    "task512_twitter_emotion_classification": ['joy', 'love']
+}
+
+# eval function
+def do_eval(checkpoint_path):
+    model = T5ModelConfig(
+        model_str=args.model_name, 
+        checkpoint_path=checkpoint_path, 
+        from_pretrained=True, 
+        use_fp16=not args.fp32, 
+        gradient_checkpoint=False, 
+    )
+
+    eval_dataset_config = NatInstSeq2SeqJSONConfig(
+        jsonl_path=import_path,
+        enc_len=1024,
+        dec_len=128,
+        data_setting=data_setting,
+        add_ar_sentinal=False, 
+        target_prepend_pad=True, 
+        model_tokenizer=model
+    )
+
+    eval_dataset = eval_dataset_config.unroll(metaconfig)
+
+    inference_config = TKInferenceConfig(
+        model=model, 
+        pjit=True, 
+        verbose=True, 
+    )
+
+    inference, _, mesh = inference_config.unroll(metaconfig)
+
+    steps_per_epoch = int(math.ceil(len(eval_dataset) / args.batch_size))
+
+    inputs = []
+    predictions = []
+    dataset = []
+    label_logprobs = []
+
+    rng = jax.random.PRNGKey(args.seed)
+
+    with mesh:
+        d = dataloader(None, eval_dataset, args.batch_size, trunc=False)
+        for batch_idx, (items, _) in tqdm(enumerate(d), total=steps_per_epoch, disable=jax.process_index() > 0):
+            if args.early_stop is not None and batch_idx * args.batch_size > args.early_stop:
+                break
+
+            rng, new_rng = jax.random.split(rng)
+
+            model_inputs = inference.tokenizer.batch_decode(items['input_ids'], skip_special_tokens=True)
+
+            inputs.extend(model_inputs)
+
+            if use_batched:
+                batch_inputs = []
+                batch_cands = []
+                cand_spans = []
+                batch_dataset = []
+
+                for i in range(len(items['input_ids'])):
+                    real_idx = batch_idx * args.batch_size + i
+                    example = dataset_jsonl[real_idx]
+
+                    if len(example['label_space']) < 2:
+                        print('WARNING: size of label space for %s is %d' % (example['id'], len(example['label_space'])))
+
+                    span = 0
+
+                    for label_cand in example['label_space']:
+                        batch_inputs.append(model_inputs[i])
+                        batch_cands.append(label_cand)
+                        batch_dataset.append(example)
+                        span += 1
+
+                    cand_spans.append(span)
+
+                #batch_inputs = batch_inputs[:64]
+                #batch_cands = batch_cands[:64]
+                #batch_dataset = batch_dataset[:64]
+
+                log_probs = inference.eval_log_probs_from_str(
+                    batch_inputs,
+                    batch_cands,
+                    eval_dataset_config.enc_len,
+                    eval_dataset_config.dec_len
+                ).log_probs
+
+                #print(batch_inputs)
+                #print(batch_cands)
+                #print(cand_spans)
+                #print(log_probs)
+
+                cand_idx = 0
+                for span in cand_spans:
+                    ranked_labels = []
+
+                    #if cand_idx + span - 1 >= len(batch_cands):
+                    #    break
+
+                    example = None
+
+                    for cand_real_idx in range(cand_idx, cand_idx + span):
+                        #print(cand_real_idx, cand_idx)
+
+                        if example is None:
+                            example = batch_dataset[cand_real_idx]
+                        else:
+                            assert batch_dataset[cand_real_idx]['id'] == example['id']
+
+                        ranked_labels.append((batch_cands[cand_real_idx], log_probs[cand_real_idx]))
+
+                        cand_idx += 1
+
+                    #print(ranked_labels)
+
+                    best_label = max(ranked_labels, key=lambda x: x[1])
+
+                    print()
+                    print(example['Task'])
+                    print(example['Instance']['input'])
+
+                    print(best_label)
+
+                    predictions.append(best_label[0])
+
+                    #dataset.append(example)
+            else:
+                for i in range(len(items['input_ids'])):
+                    real_idx = batch_idx * args.batch_size + i
+                    example = dataset_jsonl[real_idx]
+
+                    if len(example['label_space']) < 2:
+                        print('WARNING: size of label space for %s is %d' % (example['id'], len(example['label_space'])))
+
+                    ranked_labels = []
+
+                    for label_cand in example['label_space']:
+                        log_probs = inference.eval_log_probs_from_str(
+                            [model_inputs[i]],
+                            [label_cand],
+                            eval_dataset_config.enc_len,
+                            eval_dataset_config.dec_len
+                        ).log_probs[0]
+
+                        ranked_labels.append((label_cand, log_probs.item()))
+
+                    #print(ranked_labels)
+
+                    best_label = max(ranked_labels, key=lambda x: x[1])
+
+                    print()
+                    print(example['Task'])
+                    print(example['Instance']['input'])
+
+                    print(best_label)
+
+                    predictions.append(best_label[0])
+
+                    #label_logprobs.append((l, np.array(p) for l, p in ranked_labels))
+
+    tasks = []
+    for e in dataset_jsonl:
+        if e["Task"] == "task121_atomic_question_rewriting":
+            e["Task"] = "task121_zest_question_rewriting"
+        tasks.append(e["Task"])
+
+    tasks = set(tasks)
+
+    pred_disp = []
+    eval_result = []
+
+    for t in tasks:
+        correct = 0
+        total = 0
+        for ex, pred in zip(dataset_jsonl, predictions):
+            if ex['Task'] != t:
+                continue
+
+            inst_output = ex['Instance']['output']
+            if isinstance(inst_output, list):
+                inst_output = inst_output[0]
+
+            disp = 'ID: %s\tPRED: %s\tGT: %s' % (ex['Instance']['id'], pred, inst_output)
+            pred_disp.append(disp)
+
+            if ex['Task'] in override_gt:
+                if pred in override_gt[ex['Task']]:
+                    correct += 1
+            elif pred == inst_output:
+                correct += 1
+
+            total += 1
+
+        if total == 0:
+            acc = 0
+            print('WARNING: %s - total is zero' % t)
+        else:
+            acc = correct / total
+
+        eval_result.append((t, acc, total))
+
+    return pred_disp, eval_result
+
+
+pred_disp_all = []
+
+counts_all = {}
+eval_results_all = {}
+
+def dict_equals(d1, d2):
+    for k, v in d1.items():
+        if k not in d2 or d2[k] != v:
+            return False
+
+    for k, v in d2.items():
+        if k not in d1 or d1[k] != v:
+            return False
+
+    return True
+
+def read_until_done(command):
+    process = subprocess.Popen(command, stdout=subprocess.PIPE)
+    process.wait()
+
+
+print('evaluating model_%d' % args.model_iters)
+
+if args.pull_script is not None and len(args.pull_script) > 0:
+    if not args.multihost:
+        model_suffix = str(args.model_iters)
+    else:
+        model_suffix = '%d_h%d' % (args.model_iters, jax.process_index())
+
+    pull_args = ['/bin/bash', pull_script_path, checkpoints_dir_path, args.name, model_suffix]
+    
+    print('pull script args:', pull_args)
+    read_until_done(pull_args)
+
+if not args.multihost:
+    checkpoint_path = os.path.join(checkpoints_dir_path, 'model_%d' % args.model_iters)
+else:
+    checkpoint_path = os.path.join(checkpoints_dir_path, 'model_%d_h%d' % (args.model_iters, jax.process_index()))
+
+pred_disp, eval_result = do_eval(checkpoint_path)
+
+pred_disp_all.extend(pred_disp)
+
+counts = {}
+
+for (t, acc, total) in eval_result:
+    if t not in eval_results_all:
+        eval_results_all[t] = []
+
+    eval_results_all[t].append(acc)
+
+    counts[t] = total
+
+assert counts_all == {} or dict_equals(counts_all, counts)
+
+if counts_all == {}:
+    counts_all = counts
+
+with open(generations_path, 'w') as file_out:
+    file_out.write('\n'.join(pred_disp_all))
+
+with open(evaluations_path, 'w') as file_out:
+    for task_name in sorted(list(eval_results_all.keys())):
+        task_eval_results = eval_results_all[task_name]
+        file_out.write(task_name + ' ' + str(counts_all[task_name]) + ' ' + ' '.join([str(x) for x in task_eval_results]) + '\n')
+
+if args.push_script is not None and len(args.push_script) > 0:
+    push_args = ['/bin/bash', push_script_path, checkpoints_dir_path, args.name]
+
+    print('push script args:', push_args)
+    read_until_done(push_args)
